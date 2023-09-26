c97102f1e3a250caeb7e5ff8b1b08ca92169e000
Shaunak <ssnagvenkar@gmail.com>
2023-09-26 22:41:23 +0530

    Barbara

--- code1.py
+++ code1.py
@@ -6,8 +6,19 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 print(voices[0].id)
 engine.setProperty('voice',voices[1].id)
-# engine.say("Yuvaan piso")
-# engine.runAndWait()
+
+def barbara():
+    r = sr.Recognizer()
+    with sr.Microphone() as src:
+        r.pause_threshold = 1
+        audio = r.listen(src)
+    try:
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        barbara()
+    return query
+
 
 def speak(text):
     engine.say(text)
@@ -16,28 +27,37 @@ def speak(text):
 def lis():
     r = sr.Recognizer()
     with sr.Microphone() as src:
-        print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(src)
-
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        print(f"{query}\n")
+        query = query.title()
+        if(query == 'That Is All'):
+            speak('The pleasure is all mine')
+            return 'None'
     except Exception as e:
         print('Say that again...')
-        return 'None'
+        lis()
     return query
 
 def greeting():
     hour = int(datetime.datetime.now().hour)
     minute = int(datetime.datetime.now().minute)
     if(hour>=4 and hour <12):
-        speak("Good Morning")
+        speak("Good Morning, Its Barbara here, I Hope you are having a good day")
     elif(hour == 12 and minute==0):
-        speak("Good Day")
+        speak("Good Day, Its Barbara here, I Hope you are having a good day")
+    elif(hour<=16):
+        speak("Good Afternoon, Its Barbara here, I Hope you are having a good day")
     else:
-        speak("Wank off")
+       speak("Good Evening, Its Barbara here, I Hope you are having a good day")
+
 if __name__=="__main__":
-    # greeting()
-    lis()
+    print("Waiting...")
+    query = barbara()
+    query = query.title()
+    if(query == 'Hello Barbara'):
+        greeting()
+        query = lis()
