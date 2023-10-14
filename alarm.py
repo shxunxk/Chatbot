1ed35b1dfbfb990f732c970b7a59d5f83109ceee
Shaunak <ssnagvenkar@gmail.com>
2023-10-14 16:36:52 +0530

    Music

--- alarm.py
+++ alarm.py
@@ -1,41 +1,16 @@
-import time
-import threading
-import datetime
+import subprocess
 from Barbara import speak, lis
 
-alarm_timer = None
-
 def set_alarm(query):
-    global alarm_timer
+    speak('What time do you want to set the alarm for?')
+    alarm_time = lis()
     if 'set alarm' in query:
-        speak("What time would you like to set the alarm for? Please specify the time in HH:MM format.")
-        alarm_time = lis()
         try:
-            alarm_hour, alarm_minute = map(int, alarm_time.split(":"))
-            now = datetime.datetime.now()
-            alarm_datetime = now.replace(hour=alarm_hour, minute=alarm_minute, second=0, microsecond=0)
-            if alarm_datetime <= now:
-                alarm_datetime += datetime.timedelta(days=1)
-            time_difference = (alarm_datetime - now).total_seconds()
-            if alarm_timer is not None:
-                alarm_timer.cancel()
-            alarm_timer = threading.Timer(time_difference, alarm_callback)
-            alarm_timer.start()
+            command = f"New-ScheduledTaskTrigger -Once -At {alarm_time} ; Register-ScheduledTask -Trigger $_ -TaskName 'MyAlarm'"
+            subprocess.run(["powershell", "-Command", command])
             speak(f"Alarm set for {alarm_time}.")
-        except ValueError:
-            speak("Sorry, I couldn't understand the time. Please specify the time in HH:MM format.")
+        except Exception as e:
+            speak("Sorry, I couldn't set the alarm. Please try again")
 
 def delete_alarm(query):
-    global alarm_timer
-    if 'delete alarm' in query:
-        speak("Are you sure you want to delete the alarm? (Yes or No)")
-        response = lis()
-        if response.lower() == "yes":
-            if alarm_timer is not None:
-                alarm_timer.cancel()
-                alarm_timer = None
-                speak("The alarm has been deleted.")
-            else:
-                speak("There is no active alarm to delete.")
-        else:
-            speak("The alarm has not been deleted.")
+    
--- api.py
+++ api.py
@@ -1,2 +1,3 @@
 apikey_cohere = 'wbivmDuH6vZ91C88tejIEWq5uvzshb7mFFf9huvu'
 apikey_news = '25cebbe2873e43e88d0412d3f10902da'
+apikey_music = 'AIzaSyAhjY4zV7SfEAKs5prc3U_qCmB_bTv8uUk'
